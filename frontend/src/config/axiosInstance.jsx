1fe9f2af0d3e99980d5a2b5b68a143452a22a58a
Gokulakumar <gokulakumar2005@gmail.com>
2026-04-18 08:52:45 +0530

    feat: initialize axios instance with base URL from environment variables

--- frontend/src/config/axiosInstance.jsx
+++ frontend/src/config/axiosInstance.jsx
@@ -1,13 +1,13 @@
-// import axios from "axios";
-// const axiosInstance = axios.create({
-//   baseURL: "http://localhost:3030"
-// });
-// export default axiosInstance;
-
-
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL || "";
 const axiosInstance = axios.create({
-  baseURL: apiUrl.startsWith("http") ? apiUrl : `https://${apiUrl}`
+  baseURL: import.meta.env.VITE_API_URL || "" 
 });
 export default axiosInstance;
+
+
+// import axios from "axios";
+// const apiUrl = import.meta.env.VITE_API_URL || "";
+// const axiosInstance = axios.create({
+//   baseURL: apiUrl.startsWith("http") ? apiUrl : `https://${apiUrl}`
+// });
+// export default axiosInstance;
