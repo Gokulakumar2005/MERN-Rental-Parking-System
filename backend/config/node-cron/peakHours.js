ca51e4277d7fbecfd0e34a22e23557e739819f26
Gokulakumar <gokulakumar2005@gmail.com>
2026-09-23 15:22:21 +0530

    updating peak hours functionality

--- backend/config/node-cron/peakHours.js
+++ backend/config/node-cron/peakHours.js
@@ -3,7 +3,7 @@ import { BookingModel } from "../../app/models/BookingModel.js";
 import { SlotModel } from "../../app/models/ParkingSlot.js";
 
 
-cron.schedule("55 19 * * *", async () => {
+cron.schedule("59 23 * * *", async () => {
 
   try {
 
